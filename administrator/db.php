78fe536151ae64e36ac29ef26b700f1103e17dcf
Luciano Hayer <lucianohayer@gmail.com>
2022-04-22 02:30:30 +0200

    change db

--- administrator/db.php
+++ administrator/db.php
@@ -5,7 +5,7 @@ class Db{
 
     public static function getConexion(){
         if(self::$con==null){
-            self::$con=new mysqli("localhost", "root", "", "academia");
+            self::$con=new mysqli("localhost", "root", "", "wordpress25");
             if(self::$con->connect_errno){
                 echo "Fallo al conectar a MySQL: (" . self::$con->connect_errno . ") " . self::$con->connect_error;
             }
